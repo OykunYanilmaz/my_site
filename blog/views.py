6334f8fa7fb0e7c26ac4cb95d8f7f618a1874675
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-12 13:34:53 +0300

    Adding a comment mechanism

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.views import View
 from .models import Post
+from .forms import CommentForm
 
 # def get_date(post):
 #     return post['date']
@@ -50,11 +54,43 @@ class AllPostsView(ListView):
 #     })
 
 
-class SinglePostView(DetailView):
+class SinglePostViewOld(DetailView):
     template_name = "blog/post-detail.html"
     model = Post
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["post_tags"] = self.object.tags.all()
+        context["comment_form"] = CommentForm()
         return context
+
+
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context)
