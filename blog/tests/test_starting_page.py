8db2e9382348845fc4d50db8db0436dd2e8a09d2
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-13 11:45:47 +0300

    Adding a new test for starting page

--- blog/tests/test_starting_page.py
+++ blog/tests/test_starting_page.py
@@ -2,6 +2,7 @@ import pytest
 from django.urls import reverse
 from http import HTTPStatus
 from blog.models import Post, Tag
+from datetime import date, timedelta
 
 def create_post(title, slug, tag):
     post = Post.objects.create(
@@ -31,7 +32,6 @@ class TestStartingPage:
 
     def test_limits_posts_to_three(self, client):
         tag = Tag.objects.create(caption="general")
-
         for i in range(4):
             create_post(
                 title=f"Post {i}",
@@ -42,3 +42,24 @@ class TestStartingPage:
         response = client.get(reverse("starting-page"))
 
         assert len(response.context["posts"]) == 3
+
+    def test_shows_latest_three_posts_by_date(self, client, db):
+        tag = Tag.objects.create(caption="general")
+        p1 = create_post("Post 1", "post-1", tag)
+        p2 = create_post("Post 2", "post-2", tag)
+        p3 = create_post("Post 3", "post-3", tag)
+        p4 = create_post("Post 4", "post-4", tag)
+        p5 = create_post("Post 5", "post-5", tag)
+        today = date.today()
+        Post.objects.filter(pk=p1.pk).update(date=today - timedelta(days=4))
+        Post.objects.filter(pk=p2.pk).update(date=today - timedelta(days=3))
+        Post.objects.filter(pk=p3.pk).update(date=today - timedelta(days=2))
+        Post.objects.filter(pk=p4.pk).update(date=today - timedelta(days=1))
+        Post.objects.filter(pk=p5.pk).update(date=today)
+
+        response = client.get(reverse("starting-page"))
+
+        posts = list(response.context["posts"])
+        slugs = [p.slug for p in posts]
+        assert len(posts) == 3
+        assert slugs == ["post-5", "post-4", "post-3"]
